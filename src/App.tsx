33f1166e01edb8d908cf6990b2ce8d3957e6decc
Francisco Hodge <hello@franciscohodge.com>
2019-03-07 21:53:47 -0500

    Caret arrows

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "react-simple-keyboard/build/css/index.css";
 import './App.css';
 
 class App extends React.Component {
-  keyboard: Keyboard;
+  keyboardRef: Keyboard;
 
   state = {
     layoutName: "default",
@@ -25,7 +25,27 @@ class App extends React.Component {
      * If you want to handle the shift and caps lock buttons
      */
     if (button === "{shift}" || button === "{lock}") this.handleShift();
+
+    this.handleCaretButtons(button);
   };
+
+  handleCaretButtons = (button: string) => {
+    if(this.keyboardRef.keyboard.caretPosition === null){
+      this.keyboardRef.keyboard.caretPosition = this.keyboardRef.keyboard.getInput().length;
+    }
+
+    if (button === "{left}"){
+      if(this.keyboardRef.keyboard.caretPosition)
+        this.keyboardRef.keyboard.caretPosition--;
+    }
+
+    if (button === "{right}"){
+      if(parseInt(this.keyboardRef.keyboard.caretPosition) <  this.keyboardRef.keyboard.getInput().length)
+        this.keyboardRef.keyboard.caretPosition++;
+    }
+
+    console.log(this.keyboardRef.keyboard.caretPosition);
+  }
 
   handleShift = () => {
     let layoutName = this.state.layoutName;
@@ -42,7 +62,7 @@ class App extends React.Component {
         input: input
       },
       () => {
-        this.keyboard.setInput(input);
+        this.keyboardRef.keyboard.setInput(input);
       }
     );
   };
@@ -56,10 +76,32 @@ class App extends React.Component {
           onChange={e => this.onChangeInput(e)}
         />
         <Keyboard
-          ref={(r:any) => this.keyboard = r}
+          ref={(r:any) => this.keyboardRef = r}
           layoutName={this.state.layoutName}
           onChange={(input: string) => this.onChange(input)}
           onKeyPress={(button: string) => this.onKeyPress(button)}
+          layout={{
+            default: [
+              "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+              "{tab} q w e r t y u i o p [ ] \\",
+              "{lock} a s d f g h j k l ; ' {enter}",
+              "{shift} z x c v b n m , . / {shift}",
+              ".com @ {space} {left} {right}"
+            ],
+            shift: [
+              "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+              "{tab} Q W E R T Y U I O P { } |",
+              '{lock} A S D F G H J K L : " {enter}',
+              "{shift} Z X C V B N M < > ? {shift}",
+              ".com @ {space}"
+            ]
+          }}
+          display={{
+            "{left}": "left",
+            "{right}": "right"
+          }}
+          mergeDisplay
+          debug
         />
       </div>
     );
